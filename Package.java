b397452d14527f75a135f1f790e4bc60904ea735
anuj solania <anujsolania0507@gmail.com>
2026-04-23 21:12:14 +0530

    packages

---
+++ Package.java
@@ -0,0 +1,10 @@
+import bank.*;
+
+public class Package {
+    public static void main(String[] args) {
+        Account acc1 = new Account();
+        acc1.name = "savings";
+
+        System.out.println(acc1.name);
+    }
+}
